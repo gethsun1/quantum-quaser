2b5fce05785d46a0bd86db9c32e283766d96c0bf
Gethsun 1 <gethsun09@mail.com>
2024-11-21 00:18:19 +0300

    About section fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,23 @@
-import React from 'react';
-
 const Header = () => (
-  <header>
+  <header className="w-full fixed top-0 left-0 z-10">
     <div className="dynamic-bg text-[#F9FAFB] py-4 px-8 flex justify-between items-center">
-  {/* Personal Avatar */}
-  <div className="flex items-center space-x-4">
-    <img
-      src="../assets/gethsun.jpeg"
-      alt="Gethsun"
-      className="w-10 h-10 rounded-full border-2 border-[#F9FAFB]"
-    />
-    <h1 className="text-xl font-bold">Gethsun Misesi</h1>
-  </div>
+      {/* Personal Avatar */}
+      <div className="flex items-center space-x-4">
+        <img
+          src="../assets/gethsun.jpeg"
+          alt="Gethsun"
+          className="w-10 h-10 rounded-full border-2 border-[#F9FAFB]"
+        />
+        <h1 className="text-xl font-bold">Gethsun Misesi</h1>
+      </div>
 
-  {/* Navigation Links */}
-  <nav className="space-x-6">
-    <a href="#about" className="hover:text-[#FFC107]">About</a>
-    <a href="#experience" className="hover:text-[#FFC107]">Experience</a>
-    <a href="#contact" className="hover:text-[#FFC107]">Contact</a>
-  </nav>
-  </div>
+      {/* Navigation Links */}
+      <nav className="space-x-6">
+        <a href="#about" className="text-[#007BFF] font-bold hover:text-[#FFC107]">About</a>
+        <a href="#experience" className="text-[#007BFF] font-bold hover:text-[#FFC107]">Experience</a>
+        <a href="#contact" className="text-[#007BFF] font-bold hover:text-[#FFC107]">Contact</a>
+      </nav>
+    </div>
   </header>
 );
 
