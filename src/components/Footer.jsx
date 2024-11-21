8af979246913260a754e34559b00e57a1b082dbf
Gethsun1 <gethsun09@gmail.com>
2024-11-21 16:57:21 +0300

    TG QR  add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
-
+import tgQrCode from '../assets/tgqr.jpg'; 
 const Footer = () => (
-  <footer className="dynamic-bg text-[#F9FAFB] py-6 flex flex-col items-center space-y-4">
+  <footer className="dynamic-bg text-[#F9FAFB] py-6 flex flex-col items-center space-y-6">
     {/* Footer Text */}
-    <p>© 2024 Gethsun Misesi  All Rights Reserved.</p>
+    <p>© 2024 Gethsun Misesi. All Rights Reserved.</p>
 
     {/* Social Media Icons */}
     <div className="flex space-x-6">
@@ -35,7 +35,33 @@ const Footer = () => (
     </div>
 
     {/* Contact Information */}
-    <p className="mt-4">Contact: gethsun09@gmail.com | Wallet: 0xf672e2b5b3072b7ee79e3bddcf5907032c8d0c74</p>
+    <div className="text-center mt-4">
+      <p>
+        Contact:{" "}
+        <a
+          href="mailto:gethsun09@gmail.com"
+          className="text-[#007BFF] font-bold hover:text-[#FFC107]"
+        >
+          gethsun09@gmail.com
+        </a>
+      </p>
+      <p>
+        Wallet:{" "}
+        <span className="text-[#007BFF] font-bold hover:text-[#FFC107]">
+          0xf672e2b5b3072b7ee79e3bddcf5907032c8d0c74
+        </span>
+      </p>
+    </div>
+
+    {/* Telegram QR Code */}
+    <div className="mt-4 text-center">
+      <p className="mb-2 text-lg font-medium">Join me on Telegram:</p>
+      <img
+        src={tgQrCode} // Using the imported image
+        alt="Telegram QR Code"
+        className="w-32 h-32 hover:opacity-90 rounded-lg"
+      />
+    </div>
   </footer>
 );
 
